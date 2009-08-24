151cb1c5a9eb9d8d31c1706dae783fece5dd07a2
Antoine Pultier <antoine@blogjaune.fr>
2009-08-24 12:00:20 +0200

    Définition du nouveau protocole pour envoyer les données de jeu

--- status.rb
+++ status.rb
@@ -3,7 +3,7 @@
 class Status
 	
 # La partie n'est pas valide ou n'existe pas
-invalide = 0
+sanspartie = 0
 
 # un organisateur a crée la partie, et est en attente d'un joueur
 attente_organisateur = 1
@@ -11,23 +11,20 @@ attente_organisateur = 1
 # Un candidat se présente à l'organisateur
 presentation_candidat = 2
 
-# Les deux joueurs doivent choisir leur cartes
-choix_cartes = 3
+# Un candidat est en attente de la décision de l'organisateur
+attente_candidat = 3
 
-# L'organisateur peut jouer
-attente_organisateur = 4
-
-# L'adversaire peut jouer
-attente_adversaire = 5
+# En jeu 
+en_jeu = 4
 
 # Égalité
-egalite = 6
+egalite = 5
 
 # L'organisateur gagne la partie
-organisateur_gagnant = 7
+organisateur_gagnant = 6
 
 # L'adversaire gagne la partie
-adversaire_gagnant = 8
+adversaire_gagnant = 7
 
 end
 
